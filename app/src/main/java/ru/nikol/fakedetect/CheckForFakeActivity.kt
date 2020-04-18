d66a3ea6e5e181ed6cded25d8ef68bccfd255fee
Nikol 412 <travin.nikita@gmail.com>
2020-04-18 14:24:11 +0600

    init

---
+++ app/src/main/java/ru/nikol/fakedetect/CheckForFakeActivity.kt
@@ -0,0 +1,12 @@
+package ru.nikol.fakedetect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckForFakeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_for_fake)
+    }
+}
